b2ec19cdcc803691122fc188e06b29160833bc9b
Arbind Das <dasarbind269@gmail.com>
2025-11-22 13:48:13 +0545

    Done

---
+++ ApiGatway/src/main/java/com/gateway/ApiGatway/ApiGatwayApplication.java
@@ -0,0 +1,38 @@
+//package com.gateway.ApiGatway;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//public class ApiGatwayApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ApiGatwayApplication.class, args);
+//	}
+//
+//}
+
+package com.gateway.ApiGatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class ApiGatwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatwayApplication.class, args);
+    }
+
+    @GetMapping("/gateway-test")
+    public Map<String, String> gatewayTest() {
+        return Map.of("message", "Gateway is working");
+    }
+}
---
+++ quiz-service (1)/quiz-service/src/main/java/com/quiz/quiz_service/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.quiz.quiz_service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+
+    @GetMapping
+    public Map<String , String>test(){
+        return Map.of("message ", "its working......");
+    }
+}
